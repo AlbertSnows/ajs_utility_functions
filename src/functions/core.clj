2c48c1e8a44aed3bd6358602dc3cba1a78b948e7
Albert Snow <ajsnow2012@gmail.com>
2023-06-17 11:49:01 -0500

    minor fixes, finalizing function structure

--- src/functions/core.clj
+++ src/functions/core.clj
@@ -25,16 +25,16 @@
 
 ;((reduce_number n) check-fizz-buzz [])
 
+(defn get-value-if-in-correct-context [_ state]
+  "Given a context-value map (i.e. a data structure in the form {:context <bool> :value <data>}),
+   returns the corresponding value and short circuits. Nil otherwise.
+   Expects to be used in a reducer."
+  (let [correct-context (true? (:context state))
+        result (if correct-context (reduced (:value state)) nil)]
+    result))
+
 (defn find-first-true [states]
-  "Given a data structure in the form [{:context <bool> :value <data>} ...],
-   returns the value of the first map whose context is evaluate to true.
-   Returns nil if none are true. You can pass a map with a context of true to use a default value."
-  (reduce #(println %
-             ;if
-             ;(true? (:context %))
-             ;(:value %)
-             ;(reduced (:value %))
-             ;nil
-             )
-          states)
-  )
+  "Given an array of context-value states, returns the value of the first map whose context is evaluate to true.
+   Returns nil if none are true. You can pass a map with a context of true to use as a default value.
+   See get-value-if-in-correct-context for more details about implementation."
+  (let [result (reduce get-value-if-in-correct-context nil states)] result))
