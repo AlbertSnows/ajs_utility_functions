733c33d4ebf596616ac7f13992349fd96022aafe
Albert Snow <ajsnow2012@gmail.com>
2023-06-18 14:16:53 -0500

    adding reduce-int

--- src/functions/core.clj
+++ src/functions/core.clj
@@ -1,29 +1,39 @@
 (ns functions.core)
 
-;const reduce_number = (n) => (fn, initial_value, short_circuit = false) => {
-;let accumulator = initial_value;
-;if(short_circuit !== false) {
-;for (let i = 1; i <= n; i++) {
-;if(short_circuit(accumulator, i)) {
-;return accumulator;
-;}
-;accumulator = fn(accumulator, i);
-;}
-;} else {
-;for (let i = 1; i <= n; i++) {
-;accumulator = fn(accumulator, i);
-;}
-;}
-;
-;return accumulator;
-;};
-;
-;const find_first_true = (list_of_states) => {
-;
-;};
-;
+
+
 
 ;((reduce_number n) check-fizz-buzz [])
+
+(defn reduce-int-elsewhere
+  "An implementation of reduce-int but more explicit.
+  The purpose is exclusively for reference for other languages."
+  [n]
+  (fn [fn initial-value short-circuit]
+    ;  let accumulator = initial_value;
+    ;  if(short_circuit !== false) {
+    ;  for (let i = 1; i <= n; i++) {
+    ;  if(short_circuit(accumulator, i)) {
+    ;  return accumulator;
+    ;  }
+    ;  accumulator = fn(accumulator, i);
+    ;  }
+    ;  } else {
+    ;  for (let i = 1; i <= n; i++) {
+    ;  accumulator = fn(accumulator, i);
+    ;  }
+    ;  }
+
+    ;accumulator
+    )
+
+  )
+
+(defn reduce-int
+  "An absurdly lazy way to reduce over an integer. Note that it reduces over a list.
+   Not recommended unless you're doing this a lot. Just use range."
+  ([n f] (reduce f (range 0 n)))
+  ([n f init] (reduce f init (range 0 n))))
 
 (defn get-value-if-in-correct-context [_ state]
   "Given a context-value map (i.e. a data structure in the form {:context <bool> :value <data>}),
--- src/test/core.clj
+++ src/test/core.clj
@@ -3,8 +3,15 @@
             [functions.core :refer :all]
             [test.helpers :refer :all]))
 
-(deftest
-  fizzbuzz-test
+(deftest reduce-int-tests
+  "Testing that reduce-int works correctly."
+  (let [test-one (= 66 (reduce-int 12 +))
+        test-two (= 114 (reduce-int 15 + 9))
+        tests-passed (is (true? (and test-one test-two)))]
+    tests-passed))
+
+(deftest fizzbuzz-test
+  "Tests three different datasets against fizzbuzz"
   (let [test-data [{:n 3 :result ["1", "2", "Fizz"]}
                    {:n 5 :result ["1","2","Fizz","4","Buzz"]}
                    {:n 15 :result ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]}]
@@ -13,9 +20,9 @@
         all-tests-passed (map #(is (true? %)) array-of-results)]
     all-tests-passed))
 
-
 (comment
   (deftest sanity-success-test
+    "hey look a docstring"
     (is (= 4 (+ 2 2))))
 
   (deftest sanity-fail-test
