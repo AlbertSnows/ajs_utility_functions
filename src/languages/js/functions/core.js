ea51cd15da75044b00ff36007a604495851ea9c3
Albert Snow <ajsnow2012@gmail.com>
2023-06-22 16:58:06 -0500

    update core

--- src/languages/js/functions/core.js
+++ src/languages/js/functions/core.js
@@ -1,13 +1,19 @@
 // helpful run code plugin: https://marketplace.visualstudio.com/items?itemName=formulahendry.code-runner
-
+//todo: move above line to test
 // Example closure implementation. If you want mutable state, you need to do a deep copy to avoid referential problems.
-const outer = (a) => {
-  const array = a
-  const add_number_at_index = (index, number) => {
-    const number_at_index = number[i]
-    const new_number = number_at_index + number
-    array[i] = new_number
-    return structuredClone(array)
-  }
-  return add_number_at_index
+const scope = (initial_state, inner_function) => {
+  let outer_context = initial_state
+  return inner_function(outer_context);
 };
+
+const add_number_at_index = (array) => (index, number) => {
+  const number_at_index = number[index]
+  const new_number = number_at_index + number
+  array[index] = new_number
+  return structuredClone(array)
+}
+
+module.exports = {
+  add_number_at_index
+};
+sss
