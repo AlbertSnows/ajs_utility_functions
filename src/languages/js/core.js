e903365a68768cb43973a6e75b233d719f565e1d
Albert Snow <ajsnow2012@gmail.com>
2023-06-23 18:54:34 -0500

    adding functions for w/out lambda

--- src/languages/js/core.js
+++ src/languages/js/core.js
@@ -12,4 +12,33 @@ const add_number_at_index = (number) => (index) => (array) => {
 	return update(index, new_number, array);
 };
 
-export { multiply, add_number_at_index };
+const swap_array_contents = (array) => (_) =>  (left_side_index) => {
+	const index_of_last_element = array.length - 1;
+	const right_side_index = index_of_last_element - left_side_index;
+	const rsv = array[right_side_index];
+	const lsv = array[left_side_index];
+	array[right_side_index] = lsv;
+	array[left_side_index] = rsv;
+	return array;
+};
+
+// This is a curried, short-circuit version of reduce
+// The only difference is short_circuit, which expects
+// a function in the form (acc) => (val) => (idx) => bool
+// When the bool evaluates to true, it exits the function immediately. 
+// Note: only useful when you don't have access to lodash or ramda or something
+const short_circuit_reduce = f => short_circuit => init => coll => {
+	const r_handler = (acc, val, idx, itr) => {
+		const exit_early = short_circuit(acc)(val)(idx);
+		if(exit_early) {
+			itr.splice(1);
+			return acc;
+		}
+		return f(acc)(val)(idx);
+	};
+	return coll.slice(0).reduce(r_handler, init);
+};
+
+
+
+export { multiply, add_number_at_index, short_circuit_reduce };
--- test/languages/js/helpers.js
+++ test/languages/js/helpers.js
@@ -1,14 +1,25 @@
 // Functions written specifically for testing core functions go here
 import { swap, addIndex, reduce, reduced, join } from "ramda"; // https://ramdajs.com/docs/
-
-const swap_array_contents = (array) => (left_side_index) => {
+import { short_circuit_reduce } from "../../../src/languages/js/core.js";
+const swap_array_contents_rambda = (array) => (left_side_index) => {
 	const index_of_last_element = array.length - 1;
 	const right_side_index = index_of_last_element - left_side_index;
 	const updated_array = swap(right_side_index, left_side_index, array);
 	return updated_array;
 };
 
-const reverse_sentence = (phrase) => {
+const swap_array_contents = (array) => (_) =>  (left_side_index) => {
+	const index_of_last_element = array.length - 1;
+	const right_side_index = index_of_last_element - left_side_index;
+	const rsv = array[right_side_index];
+	const lsv = array[left_side_index];
+	array[right_side_index] = lsv;
+	array[left_side_index] = rsv;
+	return array;
+};
+
+
+const reverse_sentence_ramda = (phrase) => {
 	const phrase_as_array = phrase.split(" ");
 	const half_array_length = phrase_as_array.length / 2;
 	const reduce_while_with_index = addIndex(reduce);
@@ -21,6 +32,14 @@ const reverse_sentence = (phrase) => {
 		phrase_as_array,
 		phrase_as_array);
 	return join(" ", answer);
+};
+
+const reverse_sentence = (phrase) => {
+	const phrase_as_array = phrase.split(" ");
+	const half_array_length = phrase_as_array.length / 2;
+	const answer = 
+        short_circuit_reduce(swap_array_contents)((_) => (_) => (idx) => idx >= half_array_length)(phrase_as_array)(phrase_as_array);
+	return answer.join(" ", answer);
 };
 
 export { reverse_sentence };
