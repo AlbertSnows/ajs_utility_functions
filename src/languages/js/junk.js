dbd27a1dcdbf8a1d6b3276528564acfdfa05ea0f
Albert Snow <ajsnow2012@gmail.com>
2023-06-23 15:13:33 -0500

    fix junk

--- src/languages/js/junk.js
+++ src/languages/js/junk.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // For stuff I don't feel like I can make use of, but don't want to delete.
 
 // Scope in theory should allow you to quickly define a closure for a function
@@ -5,6 +6,6 @@
 // this function will pass that into your inner function and return whatever is the result
 // which...should be a function probably.
 const scope = (initial_state, inner_function) => {
-  let outer_context = initial_state
-  return inner_function(outer_context)
-}
+	let outer_context = initial_state;
+	return inner_function(outer_context);
+};
