cce3fc008e223b800d590256cdc2883862d93d1f
Albert Snow <ajsnow2012@gmail.com>
2023-06-23 16:28:08 -0500

    blep

--- test/languages/js/rip.js
+++ test/languages/js/rip.js
@@ -4,7 +4,7 @@ import { expect } from "chai";
 
 // https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=leetcode-75
 describe("AAAAAAAAAAAAAAAAAAAAAAAA", () => { 
-	it("should reverse the words in a string", () => {
+	it("should not be this hard to code in javascript (it it, it should, but it shouldn't) ", () => {
 		expect(1).to.equal(1); // expect will not work if you require esm in the mocharc.json for some reason
 	});
 });
