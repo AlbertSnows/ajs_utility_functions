a909397897dff5b75371d134cb910a77c47d6c8e
Albert Snow <ajsnow2012@gmail.com>
2023-06-22 23:03:35 -0500

    move stuff to junk

--- src/languages/js/core.js
+++ src/languages/js/core.js
@@ -1,25 +1,7 @@
 // Example closure implementation. If you want mutable state, you need to do a deep copy to avoid referential problems.
 
-// Scope in theory should allow you to quickly define a closure for a function
-// given initial state and a desired function (that expects to be a closure)
-// this function will pass that into your inner function and return whatever is the result
-// which...should be a function probably. 
-const scope = (initial_state, inner_function) => {
-  let outer_context = initial_state
-  return inner_function(outer_context);
-};
+const multiply = x => y => x * y;
 
-// given an array, index, and number will update at index in array
-// by taking the current value and adding the number to it. 
-// returns a deep copy, useful(?) for closure antics
-const add_number_at_index = (array) => (index, number) => {
-  const number_at_index = number[index]
-  const new_number = number_at_index + number
-  array[index] = new_number
-  return structuredClone(array)
-};
-
-module.exports = {
-  scope,
-  add_number_at_index
+export {
+  multiply
 };
