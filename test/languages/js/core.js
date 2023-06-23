60087cf94c0764e4101825a777de5f2730ee98ea
Albert Snow <ajsnow2012@gmail.com>
2023-06-23 14:07:08 -0500

    fixing reverse sentence

--- test/languages/js/core.js
+++ test/languages/js/core.js
@@ -1,33 +1,37 @@
 import { assert, expect } from "chai";
 import { map } from "ramda"; // https://ramdajs.com/docs/
-import { multiply, add_number_at_index } from "../../../src/languages/js/core.js";
-import { solve151 } from "./helpers.js";
+import {
+  multiply,
+  add_number_at_index,
+} from "../../../src/languages/js/core.js";
+import { reverse_sentence } from "./helpers.js";
 // https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=leetcode-75
 describe("151. Reverse Words in a String", () => {
-    it("should reverse the words in a string", () => {
-        const input = "the sky is blue";
-        const output = solve151(input);
-        expect(output).to.equal("blue is sky the");
-    })
-})
+  it("should reverse the words in a string", () => {
+    // adding `it()` allows vscode to pick up these functions as a test
+    const input = "the sky is blue";
+    const output = reverse_sentence(input);
+    expect(output).to.equal("blue is sky the");
+  });
+});
 
 describe("add number at index in array", () => {
-    it("should have the correct value at the specified array location", () => {
-        const result = add_number_at_index(6)(1)([3, 2, 1]);
-        expect(result).to.deep.equal([3, 8, 1]);
-    });
-})
+  it("should have the correct value at the specified array location", () => {
+    const result = add_number_at_index(6)(1)([3, 2, 1]);
+    expect(result).to.deep.equal([3, 8, 1]);
+  });
+});
 
 describe("simple multiplication test", () => {
-    it("should do simple multiplication correctly", () => {
-        const multiply_test = map(multiply(6), [1, 2, 3]);
-        expect(multiply_test).to.deep.equal([6, 12, 18]);    
-    });
+  it("should do simple multiplication correctly", () => {
+    const multiply_test = map(multiply(6), [1, 2, 3]);
+    expect(multiply_test).to.deep.equal([6, 12, 18]);
+  });
 });
 
 describe("simple addition test", () => {
-    it("should do simple addition correctly", () => {
-        assert.equal(3, 1 + 2);
-        assert.notEqual(0, 1);      
-    });
+  it("should do simple addition correctly", () => {
+    assert.equal(3, 1 + 2);
+    assert.notEqual(0, 1);
+  });
 });
--- test/languages/js/helpers.js
+++ test/languages/js/helpers.js
@@ -1,24 +1,27 @@
 // Functions written specifically for testing core functions go here
-import { swap, reduceWhile, addIndex } from "ramda"; // https://ramdajs.com/docs/
+import { swap, reduceWhile, addIndex, __, reduce, reduced, join } from "ramda"; // https://ramdajs.com/docs/
 
-// given an array of size n
-// given an index where the max value is n/2
 const swap_array_contents = (array) => (left_side_index) => {
-  const index_of_last_element = array.length - 1; // 8
+  const index_of_last_element = array.length - 1; 
   const right_side_index = index_of_last_element - left_side_index;
   const updated_array = swap(right_side_index, left_side_index, array);
   return updated_array;
 };
 
-const solve151 = (phrase) => {
+const reverse_sentence = (phrase) => {
   const phrase_as_array = phrase.split(" ");
   const half_array_length = phrase_as_array.length / 2;
-  const answer = addIndex(reduceWhile)(
-    (acc, val, idx) => idx <= half_array_length,
-    (acc, val, idx) => swap_array_contents(acc)(idx),
-    phrase_as_array, phrase_as_array);
-  return answer;
+  const reduce_while_with_index = addIndex(reduce);
+  const answer = reduce_while_with_index(
+    (acc, val, idx) => {
+      return idx >= half_array_length
+        ? reduced(acc)
+        : swap_array_contents(acc)(idx);
+    },
+    phrase_as_array,
+    phrase_as_array
+  );
+  return join(" ", answer);
 };
-console.log(solve151("the sky is blue"));
 
-export { solve151 };
+export { reverse_sentence };
