55c4e803079aa4e953fecf3503935769e37e9349
Albert Snow <ajsnow2012@gmail.com>
2023-06-23 16:26:34 -0500

    testing stuff with mocha that didn't work

---
+++ test/languages/js/rip.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import { expect } from "chai";
+// import { mocha, it } from "mocha";
+
+// https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=leetcode-75
+describe("AAAAAAAAAAAAAAAAAAAAAAAA", () => { 
+	it("should reverse the words in a string", () => {
+		expect(1).to.equal(1); // expect will not work if you require esm in the mocharc.json for some reason
+	});
+});
