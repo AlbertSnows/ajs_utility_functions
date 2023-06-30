ae74c406047c49070deced66f2364985fd05cf89
Albert Snow <ajsnow2012@gmail.com>
2023-06-30 13:38:57 -0500

    moved a bunch of stuff around and probably broke everything

---
+++ clojure/src/leetcode.clj
@@ -0,0 +1,24 @@
+(ns leetcode)
+
+; reverse vowels
+; make newspaper with asterisk
+; find all of x and y that add up to k
+
+(defn calc-sums [list]
+      (let [
+            answer "..."]
+           answer))
+
+(defn find-sums [first-list second-list sum]
+      (let [first-sums (calc-sums first-list)
+            answer "..."]
+           answer))
+
+(comment
+  (find-sums
+    [1 2 3 4]
+    [4 5 6]
+    9)
+  ; 2 3 + 4
+  ; 3 + 6
+  )
---
+++ clojure/test/helpers.clj
@@ -0,0 +1,23 @@
+(ns test.helpers
+  (:require [functions.core :refer :all]))
+
+(defn setup-check-fizzbuzz [div-by-3 div-by-5]
+  (fn [fizzbuzz-answers number]
+    (let [is-div-by-3? (div-by-3 number)
+          is-div-by-5? (div-by-5 number)
+          fizzbuzz-states [{:context (and is-div-by-3? is-div-by-5?) :value "FizzBuzz"}
+                           {:context is-div-by-5? :value "Buzz"}
+                           {:context is-div-by-3? :value "Fizz"}
+                           {:context true :value (str number)}]
+          answer-for-number (find-first-true fizzbuzz-states)]
+      (conj fizzbuzz-answers answer-for-number))))
+
+(def default-fizzbuzz
+  (let [div-by-x (fn [divisor] (fn [fizzbuzz-number] (= 0 (mod fizzbuzz-number divisor))))
+        div-by-3 (div-by-x 3)
+        div-by-5 (div-by-x 5)
+        check-fizzbuzz (setup-check-fizzbuzz div-by-3 div-by-5)]
+    check-fizzbuzz))
+
+(defn fizzbuzz [n]
+  (let [answer (reduce default-fizzbuzz [] (range 1 (+ n 1)))] answer))
