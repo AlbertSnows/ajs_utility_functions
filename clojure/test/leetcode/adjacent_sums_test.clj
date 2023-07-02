f536b0f56c2a8e5e2e2410d9229d3472b40e6cd6
Albert Snow <ajsnow2012@gmail.com>
2023-07-02 17:53:53 -0500

    fixing up top level test

--- clojure/test/leetcode/adjacent_sums_test.clj
+++ clojure/test/leetcode/adjacent_sums_test.clj
@@ -13,9 +13,9 @@
 
 (deftest adjacent-sums-test
 	(testing "Test that ensures ajacent sums at a high level works.
-						More tests should be/have been written for this."
+						More tests should be/have been written for this btw."
 		(let [test-cases [{:case [[1 2 3 4] [4 5 6] 9] :answer 4}
 											{:case [[1 1 1 1] [1 1 1] 2] :answer 17}
 											{:case [[6 5 4 5 6] [1 2 3 2 1] 12] :answer 9}]
-					passed (reduce test-adjacent-sum true test-cases)]
+					passed (is (true? (reduce test-adjacent-sum true test-cases)))]
 			passed)))
