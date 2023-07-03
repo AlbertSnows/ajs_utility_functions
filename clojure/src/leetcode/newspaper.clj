db77b51102b6ae58fce34f6739353970f7ac3bbd
Albert Snow <ajsnow2012@gmail.com>
2023-07-02 23:22:59 -0500

    adding tests + minor formatting fixes to newspaper

--- clojure/src/leetcode/newspaper.clj
+++ clojure/src/leetcode/newspaper.clj
@@ -27,76 +27,80 @@
 
 (defn zip-word-data [vec-of-words vec-of-word-counts]
 	(fn [zipped index]
-		(let [word                    (vec-of-words index)
-					wourd-count             (vec-of-word-counts index)
-					package                 {:word word :word-count wourd-count}
+		(let [word (vec-of-words index)
+					word-count (vec-of-word-counts index)
+					package {:word word :word-count word-count}
 					zipped-with-new-package (conj zipped package)]
 			zipped-with-new-package)))
 
 (defn handle-exceeded-width [reordered-words current-phrase word-count word]
 	(let [reordered-words-with-new-phrase (conj reordered-words current-phrase)
-				updated-state                   {:current-sum     word-count
-																				 :current-phrase  [word]
-																				 :reordered-words reordered-words-with-new-phrase}]
+				updated-state {:current-sum     word-count
+											 :current-phrase  [word]
+											 :reordered-words reordered-words-with-new-phrase}]
 		updated-state))
 
 (defn handle-below-or-at-width [current-phrase word current-sum word-count]
 	(let [updated-current-phrase (conj current-phrase word)
-				new-sum                (+ current-sum word-count)
-				updated-state          {:current-sum    new-sum
-																:current-phrase updated-current-phrase}]
+				new-sum (+ current-sum word-count)
+				updated-state {:current-sum    new-sum
+											 :current-phrase updated-current-phrase}]
 		updated-state))
 
 (defn reorder-words [actual-width]
 	(fn [{:keys [current-sum current-phrase reordered-words]} {:keys [word word-count]}]
 		(let [new-current-sum (+ current-sum word-count)
-					answer          (if (> new-current-sum actual-width)
-														(handle-exceeded-width reordered-words current-phrase word-count word)
-														(merge
-															{:reordered-words reordered-words}
-															(handle-below-or-at-width current-phrase word current-sum word-count)))]
+					answer (if (> new-current-sum actual-width)
+									 (handle-exceeded-width reordered-words current-phrase word-count word)
+									 (merge
+										 {:reordered-words reordered-words}
+										 (handle-below-or-at-width current-phrase word current-sum word-count)))]
 			answer)))
 
 (defn add-phrase-count [phrases phrase]
-	(let [phrase-with-count       {:phrase phrase :count (count phrase)}
+	(let [phrase-with-count {:phrase phrase :count (count phrase)}
 				phrases-with-new-phrase (conj phrases phrase-with-count)]
 		phrases-with-new-phrase))
+
 (defn add-asterisks-and-spaces [actual-width]
 	(fn [final-phrases {:keys [phrase count]}]
-		(let [space-to-fill         (- actual-width count)
-					left-spaces-count     (int (Math/floor (/ space-to-fill 2)))
-					right-spaces-count    (int (Math/ceil (/ space-to-fill 2)))
-					left-spaces           (join (repeat left-spaces-count " "))
-					right-spaces          (join (repeat right-spaces-count " "))
-					final-phrase          (str "*" left-spaces phrase right-spaces "*")
+		(let [space-to-fill (- actual-width count)
+					left-spaces-count (int (Math/floor (/ space-to-fill 2)))
+					right-spaces-count (int (Math/ceil (/ space-to-fill 2)))
+					left-spaces (join (repeat left-spaces-count " "))
+					right-spaces (join (repeat right-spaces-count " "))
+					final-phrase (str "*" left-spaces phrase right-spaces "*")
 					updated-final-phrases (conj final-phrases final-phrase)]
 			updated-final-phrases)))
 (defn make-newspaper [vec-of-words width]
-	(let [actual-width        (- width 2)
-				vec-of-word-counts  (into [] (map count vec-of-words))
-				list-of-indexes     (range 0 (count vec-of-word-counts))
+	(let [actual-width (- width 2)
+				vec-of-word-counts (into [] (map count vec-of-words))
+				list-of-indexes (range 0 (count vec-of-word-counts))
 				words-with-metadata (reduce (zip-word-data vec-of-words vec-of-word-counts) [] list-of-indexes)
 				reordered-words-result
-														(reduce (reorder-words actual-width) {:current-sum     0
-																																	:current-phrase  []
-																																	:reordered-words []} words-with-metadata)
-				reordered-words     (conj (:reordered-words reordered-words-result) (:current-phrase reordered-words-result))
+				(reduce (reorder-words actual-width) {:current-sum     0
+																							:current-phrase  []
+																							:reordered-words []} words-with-metadata)
+				reordered-words (conj (:reordered-words reordered-words-result) (:current-phrase reordered-words-result))
 				corrected-paragraph (map #(join " " %) reordered-words)
-				phrases-with-count  (reduce add-phrase-count [] corrected-paragraph)
-				finalized-phrases   (reduce (add-asterisks-and-spaces actual-width) [] phrases-with-count)
+				phrases-with-count (reduce add-phrase-count [] corrected-paragraph)
+				finalized-phrases (reduce (add-asterisks-and-spaces actual-width) [] phrases-with-count)
 				string-of-asterisks (join (repeat width "*"))
 				newspaper-paragraph (conj (reduce #(conj %1 %2) [string-of-asterisks] finalized-phrases) string-of-asterisks)]
 		newspaper-paragraph))
 
 (defn can-make-newspaper? [vec-of-words width] (not (some #(< width (count %)) vec-of-words)))
 
+(defn flatten-phrases [phrases]
+	(into [] (flatten (map #(split % #" ") phrases))))
+
 (defn try-making-a-newspaper [phrases width]
 	(let [trimmed-phrases (reduce #(conj %1 (trim %2)) [] phrases)
-				vec-of-words    (into [] (flatten (map #(split % #" ") trimmed-phrases)))
+				vec-of-words (flatten-phrases trimmed-phrases)
 				; if it has a word that's too big, hard exit
-				result          (if (can-make-newspaper? vec-of-words width)
-													(make-newspaper vec-of-words width)
-													{:error "Cannot fit largest word into newspaper. Give smaller words or larger width."})]
+				result (if (can-make-newspaper? vec-of-words width)
+								 (make-newspaper vec-of-words width)
+								 {:error "Cannot fit largest word into newspaper. Give smaller words or larger width."})]
 		result))
 
 (comment
--- clojure/test/leetcode/newspaper.clj
+++ clojure/test/leetcode/newspaper.clj
@@ -1,10 +1,82 @@
 (ns leetcode.newspaper
-  (:require [clojure.test :refer :all]
-            [leetcode.newspaper :refer :all]))
+	(:require [clojure.test :refer :all]
+						[leetcode.newspaper :refer :all]))
+
+(def default-test-case ["The quick" "brown foxjumps over" "thelazy dog yo!"])
+
+(deftest zip-word-data-test
+	(testing "Testing that zipping data correctly works given the expected data."
+		(let [vec-of-words (flatten-phrases default-test-case)
+					vec-of-word-counts (into [] (map count vec-of-words))
+					try-zipping (zip-word-data vec-of-words vec-of-word-counts)
+					result (try-zipping [] 3)
+					passed (is (= result [{:word "foxjumps" :word-count 8}]))]
+			passed)))
+
+(deftest handle-exceeded-width-test
+	(testing "Testing that the state updates correctly when we've exceeded the width"
+		(let [expected {:current-sum 3, :current-phrase ["foo"], :reordered-words [["a" "b"] ["c" "d"]]}
+					params [[["a" "b"]] ["c" "d"] 3 "foo"]
+					result (apply handle-exceeded-width params)
+					passed (is (= expected result))]
+			passed)))
+
+(deftest handle-below-or-at-width-test
+	(testing "Testing that the state updates correctly when we're below the width"
+		(let [expected {:current-sum 7, :current-phrase ["aaa" "eee" "boo"]}
+					params [["aaa" "eee"] "boo" 4 3]
+					result (apply handle-below-or-at-width params)
+					passed (is (= expected result))]
+			passed)))
+
+(deftest reorder-words-test
+	(testing "Testing that words are reordered correctly."
+		(let [params-list
+					[{:params
+						[{:current-sum     5
+							:current-phrase  ["foo"]
+							:reordered-words [["a" "b"] ["c"]]}
+						 {:word       "bar"
+							:word-count 3}]
+						:expected {:current-sum     3,
+											 :current-phrase  ["bar"],
+											 :reordered-words [["a" "b"] ["c"] ["foo"]]}}
+					 {:params
+						[{:current-sum     1
+							:current-phrase  ["foo"]
+							:reordered-words [["a" "b"] ["c"]]}
+						 {:word       "bar"
+							:word-count 3}]
+						:expected {:current-sum     4,
+											 :current-phrase  ["foo" "bar"],
+											 :reordered-words [["a" "b"] ["c"]]}}]
+					try-reordering (reorder-words 7)
+					add-test-result #(conj %1 {:expected (:expected %2)
+																		 :result   (apply try-reordering (:params %2))})
+					result (reduce add-test-result [] params-list)
+					passed (is (nil? (some #(not (= (:expected %) (:result %))) result)))]
+			passed)))
+
+(deftest add-phrase-count-test
+	(testing "Testing that appending the phrase count works fine"
+		(let [expected [{:phrase "ababa boo", :count 9} {:phrase "foo", :count 3}]
+					params [[{:phrase "ababa boo" :count 9}] "foo"]
+					result (apply add-phrase-count params)
+					passed (is (= result expected))]
+			passed)))
+
+(deftest add-asterisks-and-spaces-test
+	(testing "Testing adding spaces and asterisks."
+		(let [params [["*final phrases*"] {:phrase "foo bar" :count 7}]
+					try-adding-wrappers (add-asterisks-and-spaces 13)
+					expected ["*final phrases*" "*   foo bar   *"]
+					result (apply try-adding-wrappers params)
+					passed (is (= result expected))]
+			passed)))
+
 (deftest newspaper-test
-  (testing "Test that newspaper at the top level works"
-    (let [test-cases [{:case [[1 2 3 4] [4 5 6] 9] :answer 4}
-                      {:case [[1 1 1 1] [1 1 1] 2] :answer 17}
-                      {:case [[6 5 4 5 6] [1 2 3 2 1] 12] :answer 9}]
-          passed (is (true? (reduce #(and %1 %2) true test-cases)))]
-      passed)))
+	(testing "Test that newspaper at the top level works"
+		(let [test-cases [[" The quick  " "  brown foxjumps over " "  thelazy dog yo!"]
+											""]
+					passed (is (true? (reduce #(and %1 %2) true test-cases)))]
+			passed)))
