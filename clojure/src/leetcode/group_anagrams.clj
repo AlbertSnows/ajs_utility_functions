e1ac0274f4f261b021ce74eccdc30cb70f8cd602
Albert Snow <ajsnow2012@gmail.com>
2023-08-19 21:00:38 -0500

    solve anagram

--- clojure/src/leetcode/group_anagrams.clj
+++ clojure/src/leetcode/group_anagrams.clj
@@ -2,10 +2,10 @@
 
 ;; Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
-;; An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+;; An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
 ;; typically using all the original letters exactly once.
 
- 
+
 
 ;; Example 1:
 
@@ -22,10 +22,14 @@
 ;; Input: strs = ["a"]
 ;; Output: [["a"]]
 
+(defn get-anagram-frequencies [words]
+  (let [str-freqs (map frequencies words)
+       	;; don't need to sort, but can make it easier to reason about for educational reasons
+        ;; sorted-string-freqs (map #(into (sorted-map) %) str-freqs)
+        answer (frequencies str-freqs)]
+    answer))
 
-(comment 
-  (let [
-      		answer "todo"]
-   	answer)
+(comment
+  
 
  	)
