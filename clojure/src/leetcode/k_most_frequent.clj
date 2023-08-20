f1d7b03eb73b111493812413d478798a06dd5d14
Albert Snow <ajsnow2012@gmail.com>
2023-08-19 22:37:31 -0500

    solving k most frequent

---
+++ clojure/src/leetcode/k_most_frequent.clj
@@ -0,0 +1,43 @@
+(ns k-most-frequent
+  (:require [clojure.set :refer [map-invert]]))
+
+;; Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+;; Example 1:
+
+;; Input: nums = [1,1,1,2,2,3], k = 2
+;; Output: [1,2]
+
+;; Example 2:
+
+;; Input: nums = [1], k = 1
+;; Output: [1]
+
+ 
+
+;; Constraints:
+
+;;     1 <= nums.length <= 105
+;;     -104 <= nums[i] <= 104
+;;     k is in the range [1, the number of unique elements in the array].
+;;     It is guaranteed that the answer is unique.
+
+ 
+
+;; Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+(defn get-k-most-frequent-values [coll k]
+  (let [freqs (frequencies coll)
+        freq-to-num (map-invert freqs)
+        sorted-freqs-to-num (into (sorted-map) freq-to-num)
+        nums-ordered-by-freq-desc (reverse (vals sorted-freqs-to-num))
+        answer (take k nums-ordered-by-freq-desc)]
+    answer))
+
+(comment 
+  (get-k-most-frequent-values
+   [1 1 1 2 2 3]
+   2)
+ 	)
