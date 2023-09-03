bce03cc6a1e71e2813c91637f3d16c1e173f962b
Albert Snow <ajsnow2012@gmail.com>
2023-09-03 16:20:25 -0500

    small cleanup

--- clojure/src/leetcode/three_sum.clj
+++ clojure/src/leetcode/three_sum.clj
@@ -3,23 +3,15 @@
 
 (defn find-two-sum-for-n [sorted-list]
   (fn [{:keys [used three-sums]} fix-index]
-    (let [;; 	used #{}
-          ;; fix-index 0
-          ;; three-sums []
-          sorted-list
-          (vec sorted-list)
-          ;; (vec '(-4 -1 -1 0 1 2))
+    (let [sorted-list (vec sorted-list)
           fix (get sorted-list fix-index)
-          _ (println ["b" fix-index fix])
           sublist (subvec sorted-list (inc fix-index))
           already-used? (contains? used fix)
           sum (find-two-sum-for-target sublist fix)
           found-two-sum? (not (empty? sum))
           three-sum (conj sum fix)
           updated-success-cases (conj three-sums three-sum)
-          _ (println sublist)
           answer
-     					;; fix-index
           (cond
             already-used? {:used used :three-sums three-sums}
             found-two-sum? {:used (conj used fix)
@@ -36,5 +28,5 @@
                        {:used #{} :three-sums []}
                        indexes)
         answer result]
-    answer)
+    answer) 
   )
