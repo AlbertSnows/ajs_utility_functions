f0f99d0050d2827f0bbd32f4b194170a6e0436b6
Albert Snow <ajsnow2012@gmail.com>
2023-09-27 21:18:01 -0500

    moving stuff around

--- java_files/src/main/java/org/functions/Core.java
+++
@@ -1,9 +0,0 @@
-package org.functions;
-
-import java.util.Arrays;
-
-public class Core {
-	public static void entry(String[] args) {
-			System.out.printf("Hello and welcome!" + Arrays.toString(args));
-	}
-}
